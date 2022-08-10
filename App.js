7d03358e45b7ead7b0eb67c67b74e73a19700e86
SHERLINDHAYASR <sherlindhaya2503@gmail.com>
2022-08-10 14:53:03 -0700

    testingpull

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Addquantity from './day20/Addquantity';
+
+function App(){
+  return(<div className='app'>
+    <Addquantity/>
+  
+testing
+  </div>)
+}
+
+export default App;
+
+
